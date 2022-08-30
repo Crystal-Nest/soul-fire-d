00d7dca777ba5506950e4e32a856b9831cabe435
Crystal Spider <riccardo.chimisso@gmail.com>
2022-08-30 14:02:44 +0200

    Ported Fabric to 1.19.

---
+++ fabric/src/main/java/crystalspider/soulfired/api/FireBuilder.java
@@ -0,0 +1,359 @@
+package crystalspider.soulfired.api;
+
+import crystalspider.soulfired.api.enchantment.FireTypedArrowEnchantment;
+import crystalspider.soulfired.api.enchantment.FireTypedAspectEnchantment;
+import crystalspider.soulfired.api.type.FireTyped;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.Enchantment.Rarity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+/**
+ * Builder for {@link Fire} instances.
+ */
+public class FireBuilder {
+  /**
+   * Default value for {@link #damage}.
+   */
+  public static final float DEFAULT_DAMAGE = 1.0F;
+  /**
+   * Default value for {@link #invertHealAndHarm}.
+   */
+  public static final boolean DEFAULT_INVERT_HEAL_AND_HARM = false;
+  /**
+   * Default value for {@link #inFire}.
+   */
+  public static final DamageSource DEFAULT_IN_FIRE = DamageSource.IN_FIRE;
+  /**
+   * Default value for {@link #onFire}.
+   */
+  public static final DamageSource DEFAULT_ON_FIRE = DamageSource.ON_FIRE;
+  /**
+   * Default value for {@link #hurtSound}.
+   */
+  public static final SoundEvent DEFAULT_HURT_SOUND = SoundEvents.ENTITY_PLAYER_HURT_ON_FIRE;
+  /**
+   * Default value for {@link #blockState}.
+   */
+  public static final BlockState DEFAULT_BLOCKSTATE = Blocks.FIRE.getDefaultState();
+
+  /**
+   * {@link Fire} instance {@link Fire#modId modId}.
+   * <p>
+   * Required.
+   */
+  private String modId;
+  /**
+   * {@link Fire} instance {@link Fire#id id}.
+   * <p>
+   * Required.
+   */
+  private String id;
+
+  /**
+   * {@link Fire} instance {@link Fire#damage damage}.
+   * <p>
+   * Optional, defaults to {@code 1.0F}.
+   */
+  private float damage;
+
+  /**
+   * {@link Fire} instance {@link Fire#invertHealAndHarm invertedHealAndHarm}.
+   * <p>
+   * Optional, defaults to {@code false}.
+   */
+  private boolean invertHealAndHarm;
+
+  /**
+   * {@link Fire} instance {@link Fire#inFire inFire}.
+   * <p>
+   * Optional, defaults to {@link DamageSource#IN_FIRE}.
+   * <p>
+   * If changed from the default, remember to add translations for the new death messages!
+   */
+  private DamageSource inFire;
+  /**
+   * {@link Fire} instance {@link Fire#onFire onFire}.
+   * <p>
+   * Optional, defaults to {@link DamageSource#ON_FIRE}.
+   * <p>
+   * If changed from the default, remember to add translations for the new death messages!
+   */
+  private DamageSource onFire;
+
+  /**
+   * {@link Fire} instance {@link Fire#hurtSound hurtSound}.
+   * <p>
+   * Optional, defaults to {@link SoundEvents#PLAYER_HURT_ON_FIRE}.
+   * <p>
+   * Default value is recommended.
+   */
+  private SoundEvent hurtSound;
+
+  /**
+   * {@link Fire} instance {@link Fire#blockState blockState}.
+   * <p>
+   * Optional, defaults to {@code Blocks.FIRE.defaultBlockState()}.
+   * <p>
+   * Default value is <b>not</b> recommended.
+   */
+  private BlockState blockState;
+
+  /**
+   * {@link Fire} instance {@link Fire#fireAspect fireAspect}.
+   * <p>
+   * Optional, defaults to a new {@link FireTypedAspectEnchantment} with {@link Rarity#VERY_RARE}.
+   */
+  private Enchantment fireAspect;
+  /**
+   * {@link Fire} instance {@link Fire#flame flame}.
+   * <p>
+   * Optional, defaults to a new {@link FireTypedArrowEnchantment} with {@link Rarity#VERY_RARE}.
+   */
+  private Enchantment flame;
+
+  FireBuilder() {
+    reset();
+  }
+
+  /**
+   * Sets the {@link #modId}.
+   * 
+   * @param modId
+   * @return this Builder to either set other properties or {@link #build}.
+   */
+  public FireBuilder setModId(String modId) {
+    this.modId = modId;
+    return this;
+  }
+
+  /**
+   * Sets the {@link #id}.
+   * <p>
+   * {@link #id} will be set only if the given {@code id} is valid. In addition, a valid {@code id} will be trimmed.
+   * 
+   * @param id
+   * @return this Builder to either set other properties or {@link #build}.
+   */
+  public FireBuilder setId(String id) {
+    if (FireManager.isValidFireId(id)) {
+      this.id = id;
+    }
+    return this;
+  }
+
+  /**
+   * Sets the {@link #damage}.
+   * <p>
+   * If the {@code damage} passed is {@code >= 0} the fire will harm entities, otherwise it will heal them.
+   * <p>
+   * Whether the fire heals or harms depends also on {@link #invertHealAndHarm}.
+   * 
+   * @param damage
+   * @return this Builder to either set other properties or {@link #build}.
+   */
+  public FireBuilder setDamage(float damage) {
+    this.damage = damage;
+    return this;
+  }
+  
+  /**
+   * Sets the {@link #invertHealAndHarm} flag.
+   * <p>
+   * If set to true, entities that have heal and harm inverted (e.g. undeads) will have heal and harm inverted for this fire too.
+   * 
+   * @param invertHealAndHarm
+   * @return this Builder to either set other properties or {@link #build}.
+   */
+  public FireBuilder setInvertHealAndHarm(boolean invertHealAndHarm) {
+    this.invertHealAndHarm = invertHealAndHarm;
+    return this;
+  }
+
+  /**
+   * Sets the {@link DamageSource} {@link #inFire}.
+   * <p>
+   * Prefer {@link #setInFire(String)}.
+   * 
+   * @param inFire
+   * @return this Builder to either set other properties or {@link #build}.
+   */
+  private FireBuilder setInFire(DamageSource inFire) {
+    this.inFire = inFire;
+    return this;
+  }
+
+  /**
+   * Sets the {@link DamageSource} {@link #inFire} by creating a new {@link DamageSource} with the given {@code messageId}.
+   * <p>
+   * The {@link DamageSource} will have both {@link DamageSource#bypassArmor bypassArmor} and {@link DamageSource#isFireSource isFireSource} set to {@code true}, and {@link DamageSource#exhaustion exhaustion} set to {@code 0.0F}.
+   * 
+   * @param messageId
+   * @return this Builder to either set other properties or {@link #build}.
+   */
+  public FireBuilder setInFire(String name) {
+    return setInFire((new FireDamageSource(name)).setBypassesArmor().setFire());
+  }
+
+  /**
+   * Sets the {@link DamageSource} {@link #inFire} by creating a new {@link DamageSource} with the {@code messageId} equal to {@code "in_" + id + "_fire"}.
+   * 
+   * @return this Builder to either set other properties or {@link #build}.
+   * @throws IllegalStateException if the {@link #id} is invalid (not set or blank).
+   */
+  public FireBuilder setInFire() throws IllegalStateException {
+    if (FireManager.isValidFireId(id)) {
+      return setInFire("in_" + id + "_fire");
+    }
+    throw new IllegalStateException("Attempted to create inFire DamageSource before setting a valid id");
+  }
+
+  /**
+   * Sets the {@link DamageSource} {@link #onFire}.
+   * <p>
+   * Prefer {@link #setOnFire(String)}.
+   * 
+   * @param onFire
+   * @return this Builder to either set other properties or {@link #build}.
+   */
+  private FireBuilder setOnFire(DamageSource onFire) {
+    this.onFire = onFire;
+    return this;
+  }
+
+  /**
+   * Sets the {@link DamageSource} {@link #onFire} by creating a new {@link DamageSource} with the given {@code messageId}.
+   * <p>
+   * The {@link DamageSource} will have both {@link DamageSource#bypassArmor bypassArmor} and {@link DamageSource#isFireSource isFireSource} set to {@code true}, and {@link DamageSource#exhaustion exhaustion} set to {@code 0.0F}.   * 
+   * 
+   * @param messageId
+   * @return this Builder to either set other properties or {@link #build}.
+   */
+  public FireBuilder setOnFire(String name) {
+    return setOnFire((new FireDamageSource(name)).setBypassesArmor().setFire());
+  }
+
+  /**
+   * Sets the {@link DamageSource} {@link #inFire} by creating a new {@link DamageSource} with the {@code messageId} equal to {@code "on_" + id + "_fire"}.
+   * 
+   * @return this Builder to either set other properties or {@link #build}.
+   * @throws IllegalStateException if the {@link #id} is invalid (not set or blank).
+   */
+  public FireBuilder setOnFire() throws IllegalStateException {
+    if (FireManager.isValidFireId(id)) {
+      return setOnFire("on_" + id + "_fire");
+    }
+    throw new IllegalStateException("Attempted to create onFire DamageSource before setting a valid id");
+  }
+
+  /**
+   * Sets the {@link #hurtSound}.
+   * 
+   * @param hurtSound
+   * @return this Builder to either set other properties or {@link #build}.
+   */
+  public FireBuilder setHurtSound(SoundEvent hurtSound) {
+    this.hurtSound = hurtSound;
+    return this;
+  }
+
+  /**
+   * Sets the {@link #blockState}.
+   * <p>
+   * Use this only if you need fine grained control on the BlockState associated to the Fire, otherwise prefer {@link #setSourceBlock(BaseFireBlock)}
+   * 
+   * @param blockState
+   * @return this Builder to either set other properties or {@link #build}.
+   */
+  public FireBuilder setSourceBlock(BlockState blockState) {
+    this.blockState = blockState;
+    return this;
+  }
+
+  /**
+   * Sets the {@link #blockState} to the {@link Block#getDefaultState() default BlockState} of the given {@code sourceBlock}.
+   * 
+   * @param sourceBlock
+   * @return this Builder to either set other properties or {@link #build}.
+   */
+  public FireBuilder setSourceBlock(Block sourceBlock) {
+    return setSourceBlock(sourceBlock.getDefaultState());
+  }
+
+  /**
+   * Sets the {@link #fireAspect} enchantment.
+   * <p>
+   * The {@link Enchantment} passed as parameter MUST implement the {@link FireTyped} interface, otherwise the value will not be set.
+   * 
+   * @param fireAspect
+   * @return this Builder to either set other properties or {@link #build}.
+   */
+  public FireBuilder setFireAspect(Enchantment fireAspect) {
+    if (flame instanceof FireTyped) {
+      this.fireAspect = fireAspect;
+    }
+    return this;
+  }
+
+  /**
+   * Sets the {@link #fireAspect} enchantment.
+   * <p>
+   * The {@link Enchantment} passed as parameter MUST implement the {@link FireTyped} interface, otherwise the value will not be set.
+   * 
+   * @param fireAspect
+   * @return this Builder to either set other properties or {@link #build}.
+   */
+  public FireBuilder setFlame(Enchantment flame) {
+    if (flame instanceof FireTyped) {
+      this.flame = flame;
+    }
+    return this;
+  }
+
+  /**
+   * Resets the state of the Builder.
+   * <p>
+   * Used to avoid getting new Builders from the {@link FireManager manager} and instead use the same instance to build different {@link Fire Fires}.
+   * 
+   * @return this Builder, reset.
+   */
+  public FireBuilder reset() {
+    id = null;
+    damage = DEFAULT_DAMAGE;
+    invertHealAndHarm = DEFAULT_INVERT_HEAL_AND_HARM;
+    inFire = DEFAULT_IN_FIRE;
+    onFire = DEFAULT_ON_FIRE;
+    hurtSound = DEFAULT_HURT_SOUND;
+    blockState = DEFAULT_BLOCKSTATE;
+    fireAspect = null;
+    flame = null;
+    return this;
+  }
+
+  /**
+   * Build a {@link Fire} instance.
+   * <p>
+   * If {@link #fireAspect} is not set, it will default to a new {@link FireTypedAspectEnchantment} with {@link Rarity#VERY_RARE}.
+   * <p>
+   * If {@link #flame} is not set, it will default to a new {@link FireTypedArrowEnchantment} with {@link Rarity#VERY_RARE}.
+   * 
+   * @return {@link Fire} instance.
+   * @throws IllegalStateException if the {@link #id} is invalid (not set or blank).
+   */
+  public Fire build() throws IllegalStateException {
+    if (FireManager.isValidFireId(id) && !(modId == null || modId.isBlank())) {
+      if (fireAspect == null) {
+        fireAspect = new FireTypedAspectEnchantment(modId, id, Rarity.VERY_RARE);
+      }
+      if (flame == null) {
+        flame = new FireTypedArrowEnchantment(modId, id, Rarity.VERY_RARE);
+      }
+      return new Fire(modId, FireManager.sanitizeFireId(id), damage, invertHealAndHarm, inFire, onFire, hurtSound, blockState, fireAspect, flame);
+    }
+    throw new IllegalStateException("Attempted to build a Fire with a non-valid id or modId");
+  }
+}
---
+++ fabric/src/main/java/crystalspider/soulfired/mixin/EntityMixin.java
@@ -0,0 +1,143 @@
+package crystalspider.soulfired.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import crystalspider.soulfired.api.FireManager;
+import crystalspider.soulfired.api.type.FireTypeChanger;
+import crystalspider.soulfired.api.type.FireTyped;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.World;
+
+/**
+ * Injects into {@link Entity} to alter Fire behavior for consistency.
+ */
+@Mixin(Entity.class)
+public abstract class EntityMixin implements FireTypeChanger {
+  /**
+   * Shadowed {@link Entity#world}.
+   */
+  @Shadow
+  public World world;
+  /**
+   * Shadowed {@link Entity#dataTracker}.
+   */
+  @Shadow
+  protected DataTracker dataTracker;
+
+  /**
+   * {@link TrackedData} to synchronize the Fire Id across client and server.
+   */
+  private static final TrackedData<String> DATA_FIRE_ID = DataTracker.registerData(Entity.class, TrackedDataHandlerRegistry.STRING);
+
+  /**
+   * Shadowed {@link Entity#initDataTracker()}.
+   */
+  @Shadow
+  protected abstract void initDataTracker();
+  /**
+   * Shadowed {@link Entity#getFireTicks()}.
+   * 
+   * @return the remaining ticks the entity is set to burn for.
+   */
+  @Shadow
+  public abstract int getFireTicks();
+  /**
+   * Shadowed {@link Entity#isInLava()}.
+   * 
+   * @return whether this entity is in lava.
+   */
+  @Shadow
+  public abstract boolean isInLava();
+
+  @Override
+  public void setFireId(String fireId) {
+    dataTracker.set(DATA_FIRE_ID, fireId != null ? fireId.trim() : "");
+  }
+
+  @Override
+  public String getFireId() {
+    return dataTracker.get(DATA_FIRE_ID);
+  }
+
+  /**
+   * Redirects the call to {@link Entity#initDataTracker()} inside the constructor.
+   * <p>
+   * Defines the {@link #DATA_FIRE_ID Fire Id data} to synchronize across client and server.
+   * 
+   * @param caller {@link Entity} invoking (owning) the redirected method. It's the same as {@code this} entity.
+   */
+  @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;initDataTracker()V"))
+  private void redirectInitDataTracker(Entity caller) {
+    dataTracker.startTracking(DATA_FIRE_ID, "");
+    initDataTracker();
+  }
+
+  /**
+   * Redirects the call to {@link Entity#damage(DamageSource, float)} inside the method {@link Entity#baseTick()}.
+   * <p>
+   * Hurts the entity with the correct fire damage and {@link DamageSource}.
+   * 
+   * @param caller {@link Entity} invoking (owning) the redirected method. It's the same as this entity.
+   * @param damageSource original {@link DamageSource} (normale fire).
+   * @param damage original damage (normal fire).
+   * @return the result of calling the redirected method.
+   */
+  @Redirect(method = "baseTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
+  private boolean redirectDamage(Entity caller, DamageSource damageSource, float damage) {
+    return FireManager.damageOnFire(caller, ((FireTyped) caller).getFireId(), damageSource, damage);
+  }
+
+  /**
+   * Injects at the start of the method {@link Entity#setFireTicks(int)}.
+   * <p>
+   * Resets the FireId when this entity stops burning or catches fire from a new fire source.
+   * 
+   * @param ticks ticks this entity should burn for.
+   * @param ci {@link CallbackInfo}.
+   */
+  @Inject(method = "setFireTicks", at = @At(value = "HEAD"))
+  private void onSetFireTicks(int ticks, CallbackInfo ci) {
+    if (!world.isClient && (ticks <= 0 || ticks >= getFireTicks())) {
+      setFireId(FireManager.BASE_FIRE_ID);
+    }
+  }
+
+  /**
+   * Injects in the method {@link Entity#writeNbt(NbtCompound)} before the invocation of {@link Entity#writeCustomDataToNbt(NbtCompound)}.
+   * <p>
+   * If valid, saves the current FireId in the given {@link NbtCompound}.
+   * 
+   * @param tag
+   * @param cir {@link CallbackInfoReturnable}.
+   */
+  @Inject(method = "writeNbt", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;writeCustomDataToNbt(Lnet/minecraft/nbt/NbtCompound;)V"))
+  private void onWriteNbt(NbtCompound tag, CallbackInfoReturnable<NbtCompound> cir) {
+    if (FireManager.isFireId(getFireId())) {
+      tag.putString("FireId", getFireId());
+    }
+  }
+
+  /**
+   * Injects in the method {@link Entity#readNbt(NbtCompound)} before the invocation of {@link Entity#readCustomDataFromNbt(NbtCompound)}.
+   * <p>
+   * Loads the FireId from the given {@link NbtCompound}.
+   * 
+   * @param tag
+   * @param ci {@link CallbackInfo}.
+   */
+  @Inject(method = "readNbt", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;readCustomDataFromNbt(Lnet/minecraft/nbt/NbtCompound;)V"))
+  private void onReadNbt(NbtCompound tag, CallbackInfo ci) {
+    setFireId(FireManager.ensureFireId(tag.getString("FireId")));
+  }
+}
