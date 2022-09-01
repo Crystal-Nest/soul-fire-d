38f8b3cee103108663c4967f4c47954de79d3b7b
Crystal Spider <riccardo.chimisso@gmail.com>
2022-09-01 20:00:50 +0200

    Fixed possible crash Forge.

--- forge/src/main/java/crystalspider/soulfired/mixin/PlayerEntityMixin.java
+++ forge/src/main/java/crystalspider/soulfired/mixin/PlayerEntityMixin.java
@@ -3,7 +3,7 @@ package crystalspider.soulfired.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import crystalspider.soulfired.api.FireManager;
@@ -11,28 +11,16 @@ import crystalspider.soulfired.api.type.FireTyped;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
-import net.minecraft.world.World;
 
 /**
  * Injects into {@link PlayerEntity} to alter Fire behavior for consistency.
  */
 @Mixin(PlayerEntity.class)
-public abstract class PlayerEntityMixin extends LivingEntity implements FireTyped {
-  /**
-   * Useless constructor required by the super class to make the compiler happy.
-   * 
-   * @param entityType
-   * @param world
-   */
-  private PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
-    super(entityType, world);
-  }
-
+public abstract class PlayerEntityMixin implements FireTyped {
   /**
    * Injects at the start of the method {@link PlayerEntity#getHurtSound(DamageSource)}.
    * <p>
@@ -49,18 +37,18 @@ public abstract class PlayerEntityMixin extends LivingEntity implements FireType
   }
 
   /**
-   * Modifies the assignment value returned by {@link EnchantmentHelper#getFireAspect(LivingEntity)} in the method {@link PlayerEntity#attack(Entity)}.
+   * Redirects the call to the method {@link EnchantmentHelper#getFireAspect(LivingEntity)} in the method {@link PlayerEntity#attack(Entity)}.
    * <p>
    * Returns the proper Fire Aspect level taking into account all possible Fire Aspect enchantments.
    * 
-   * @param level level the basic Fire Aspect enchantment is at.
+   * @param player
    */
-  @ModifyVariable(method = "attack", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/enchantment/EnchantmentHelper;getFireAspect(Lnet/minecraft/entity/LivingEntity;)I"))
-  private int onAttack(int level) {
-    int fireAspectLevel = level;
+  @Redirect(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/EnchantmentHelper;getFireAspect(Lnet/minecraft/entity/LivingEntity;)I"))
+  private int onAttack(LivingEntity player) {
+    int fireAspectLevel = EnchantmentHelper.getFireAspect(player);
     if (fireAspectLevel <= 0) {
       for (Enchantment fireAspect : FireManager.getFireAspects()) {
-        int enchantmentLevel = EnchantmentHelper.getEnchantmentLevel(fireAspect, this);
+        int enchantmentLevel = EnchantmentHelper.getEnchantmentLevel(fireAspect, player);
         if (enchantmentLevel > 0) {
           fireAspectLevel = enchantmentLevel;
           break;
