577a13db8b80ee79d26b84ad27ce90c30a034a3a
Crystal Spider <riccardo.chimisso@gmail.com>
2022-09-24 17:49:13 +0200

    Improved logic, fixed a bug and some compatibility problems, expanded API, updated version number.
    (cherry picked from commit 4e4bab90a1c7b549974a63b59ad1bd83939497ba)

--- forge/src/main/java/crystalspider/soulfired/mixin/PlayerMixin.java
+++ forge/src/main/java/crystalspider/soulfired/mixin/PlayerMixin.java
@@ -3,18 +3,13 @@ package crystalspider.soulfired.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import crystalspider.soulfired.api.FireManager;
 import crystalspider.soulfired.api.type.FireTyped;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 
 /**
  * Injects into {@link Player} to alter Fire behavior for consistency.
@@ -29,32 +24,10 @@ public abstract class PlayerMixin implements FireTyped {
    * @param damageSource {@link DamageSource} causing the damage.
    * @param cir {@link CallbackInfoReturnable}.
    */
-  @Inject(method = "getHurtSound", at = @At(value = "HEAD"))
+  @Inject(method = "getHurtSound", at = @At(value = "HEAD"), cancellable = true)
   private void onGetHurtSound(DamageSource damageSource, CallbackInfoReturnable<SoundEvent> cir) {
-    if (FireManager.isFireDamageSource(damageSource) && cir.isCancellable()) {
+    if (FireManager.isFireDamageSource(damageSource)) {
       cir.setReturnValue(FireManager.getHurtSound(getFireId()));
     }
-  }
-
-  /**
-   * Redirects the call to the method {@link EnchantmentHelper#getFireAspect(LivingEntity)} in the method {@link Player#attack(Entity)}.
-   * <p>
-   * Returns the proper Fire Aspect level taking into account all possible Fire Aspect enchantments.
-   * 
-   * @param player
-   */
-  @Redirect(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;getFireAspect(Lnet/minecraft/world/entity/LivingEntity;)I"))
-  private int onAttack(LivingEntity player) {
-    int fireAspectLevel = EnchantmentHelper.getFireAspect(player);
-    if (fireAspectLevel <= 0) {
-      for (Enchantment fireAspect : FireManager.getFireAspects()) {
-        int enchantmentLevel = EnchantmentHelper.getEnchantmentLevel(fireAspect, player);
-        if (enchantmentLevel > 0) {
-          fireAspectLevel = enchantmentLevel;
-          break;
-        }
-      }
-    }
-    return fireAspectLevel;
   }
 }
