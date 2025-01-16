73ad92ce99cd4b56db2d6527f562525c2652fe1c
Jonathing <me@jonathing.me>
2025-01-16 03:30:20 +0100

    Use ParticleOptions for custom torches

--- common/src/main/java/it/crystalnest/soul_fire_d/api/FireManager.java
+++ common/src/main/java/it/crystalnest/soul_fire_d/api/FireManager.java
@@ -16,6 +16,8 @@ import it.crystalnest.soul_fire_d.api.block.entity.DynamicBlockEntityType;
 import it.crystalnest.soul_fire_d.api.type.FireTypeChanger;
 import it.crystalnest.soul_fire_d.api.type.FireTyped;
 import net.minecraft.core.Direction;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -323,7 +325,7 @@ public final class FireManager {
    * @param <T> particle type.
    * @return supplier for the registered particle type.
    */
-  public static <T extends SimpleParticleType> Supplier<T> registerParticle(ResourceLocation fireType, Supplier<T> supplier) {
+  public static <T extends ParticleType<?>> Supplier<T> registerParticle(ResourceLocation fireType, Supplier<T> supplier) {
     return CobwebRegistry.of(Registries.PARTICLE_TYPE, fireType.getNamespace()).register(FireManager.getComponentPath(fireType, Fire.Component.FLAME_PARTICLE), supplier);
   }
 
@@ -351,8 +353,8 @@ public final class FireManager {
    */
   public static <T extends CustomTorchBlock, W extends CustomWallTorchBlock> Pair<Supplier<T>, Supplier<W>> registerTorch(
     ResourceLocation fireType,
-    BiFunction<ResourceLocation, Supplier<SimpleParticleType>, T> torchSupplier,
-    BiFunction<ResourceLocation, Supplier<SimpleParticleType>, W> wallTorchSupplier
+    BiFunction<ResourceLocation, Supplier<? extends ParticleOptions>, T> torchSupplier,
+    BiFunction<ResourceLocation, Supplier<? extends ParticleOptions>, W> wallTorchSupplier
   ) {
     CobwebRegister<Block> blocks = CobwebRegistry.ofBlocks(fireType.getNamespace());
     return Pair.of(
@@ -475,8 +477,8 @@ public final class FireManager {
    *
    * @param fireType fire type.
    * @param getter property getter.
-   * @return property value.
    * @param <T> property type.
+   * @return property value.
    */
   public static <T> T getProperty(ResourceLocation fireType, Function<Fire, T> getter) {
     return getter.apply(getFire(fireType));
