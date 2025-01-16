98d142fb381bf86e5667ccd69f0301d8af4043bf
Jonathing <me@jonathing.me>
2025-01-16 02:36:32 +0100

    Use WrapOperation for campfire mixin, fixes #6

--- common/src/main/java/it/crystalnest/soul_fire_d/mixin/CampfireBlockMixin.java
+++ common/src/main/java/it/crystalnest/soul_fire_d/mixin/CampfireBlockMixin.java
@@ -1,5 +1,7 @@
 package it.crystalnest.soul_fire_d.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import it.crystalnest.soul_fire_d.api.Fire;
 import it.crystalnest.soul_fire_d.api.FireManager;
 import it.crystalnest.soul_fire_d.api.type.FireTypeChanger;
@@ -13,7 +15,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * Injects into {@link CampfireBlock} to alter Fire behavior for consistency.
@@ -43,9 +44,10 @@ public abstract class CampfireBlockMixin implements FireTypeChanger {
    * @param instance owner of the redirected method.
    * @param damageSource original {@link DamageSource} (normal fire).
    * @param damage original damage (normal fire).
+   * @return the result of calling the redirected method.
    */
-  @Redirect(method = "entityInside", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)V"))
-  private void redirectHurt(Entity instance, DamageSource damageSource, float damage) {
+  @WrapOperation(method = "entityInside", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)V"))
+  private void redirectHurt(Entity instance, DamageSource damageSource, float damage, Operation<Boolean> original) {
     FireManager.affect(instance, getFireType(), Fire::getOnCampfire);
   }
 }
