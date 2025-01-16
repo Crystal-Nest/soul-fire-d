ae8e2a9d1109244dd69de35f3fae3d021f4231b9
Jonathing <me@jonathing.me>
2025-01-16 10:45:27 -0500

    Change API status if some FireManager methods

--- common/src/main/java/it/crystalnest/soul_fire_d/api/FireManager.java
+++ common/src/main/java/it/crystalnest/soul_fire_d/api/FireManager.java
@@ -763,7 +763,6 @@ public final class FireManager {
    * @param seconds amount of seconds the fire should last for.
    * @param fireType fire type.
    */
-  @Deprecated(forRemoval = true)
   public static void setOnFire(Entity entity, int seconds, ResourceLocation fireType) {
     setOnFire(entity, seconds, fireType, Entity::setSecondsOnFire);
   }
@@ -776,6 +775,7 @@ public final class FireManager {
    * @param fireType fire type.
    * @param setOnFireFunction how to set the entity on fire.
    */
+  @ApiStatus.Internal
   public static void setOnFire(Entity entity, int seconds, ResourceLocation fireType, BiConsumer<Entity, Integer> setOnFireFunction) {
     setOnFireFunction.accept(entity, seconds);
     ((FireTypeChanger) entity).setFireType(ensure(fireType));
@@ -789,7 +789,6 @@ public final class FireManager {
    * @param fireType fire type.
    * @return whether the {@code entity} has been harmed.
    */
-  @Deprecated(forRemoval = true)
   public static boolean damageInFire(Entity entity, ResourceLocation fireType) {
     return damageInFire(entity, fireType, Entity::hurt);
   }
@@ -803,6 +802,7 @@ public final class FireManager {
    * @param hurtFunction how to harm the {@code entity}.
    * @return whether the {@code entity} has been harmed.
    */
+  @ApiStatus.Internal
   public static boolean damageInFire(Entity entity, ResourceLocation fireType, TriFunction<Entity, DamageSource, Float, Boolean> hurtFunction) {
     ((FireTypeChanger) entity).setFireType(ensure(fireType));
     return harmOrHeal(entity, getInFireDamageSource(fireType), FireManager.getProperty(fireType, Fire::getDamage), FireManager.getProperty(fireType, Fire::invertHealAndHarm), hurtFunction);
@@ -816,7 +816,6 @@ public final class FireManager {
    * @param fireType fire type.
    * @return whether the {@code entity} has been harmed.
    */
-  @Deprecated(forRemoval = true)
   public static boolean damageOnFire(Entity entity, ResourceLocation fireType) {
     return damageOnFire(entity, fireType, Entity::hurt);
   }
@@ -830,24 +829,10 @@ public final class FireManager {
    * @param hurtFunction how to harm the {@code entity}.
    * @return whether the {@code entity} has been harmed.
    */
+  @ApiStatus.Internal
   public static boolean damageOnFire(Entity entity, ResourceLocation fireType, TriFunction<Entity, DamageSource, Float, Boolean> hurtFunction) {
     ((FireTypeChanger) entity).setFireType(ensure(fireType));
     return harmOrHeal(entity, getOnFireDamageSource(fireType), FireManager.getProperty(fireType, Fire::getDamage), FireManager.getProperty(fireType, Fire::invertHealAndHarm), hurtFunction);
-  }
-
-  /**
-   * Harms or heals the given {@code entity}.<br />
-   * Also applies the custom fire behavior.
-   *
-   * @param entity entity to harm/heal.
-   * @param damageSource damage source.
-   * @param damage damage/heal amount.
-   * @param invertHealAndHarm whether to invert heal and harm.
-   * @return whether the {@code entity} has been harmed.
-   */
-  @Deprecated(forRemoval = true)
-  private static boolean harmOrHeal(Entity entity, DamageSource damageSource, float damage, boolean invertHealAndHarm) {
-    return harmOrHeal(entity, damageSource, damage, invertHealAndHarm, Entity::hurt);
   }
 
   /**
