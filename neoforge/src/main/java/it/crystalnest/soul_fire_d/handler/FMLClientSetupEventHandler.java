a241c9e30117d22a244c59fc65713a629255ffe8
Crystal Spider <riccardo.chimisso@gmail.com>
2025-01-17 11:01:23 +0100

    Fix NeoForge.

--- neoforge/src/main/java/it/crystalnest/soul_fire_d/handler/FMLClientSetupEventHandler.java
+++ neoforge/src/main/java/it/crystalnest/soul_fire_d/handler/FMLClientSetupEventHandler.java
@@ -13,6 +13,7 @@ import net.minecraft.client.particle.FlameParticle;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.CampfireRenderer;
+import net.minecraft.core.particles.SimpleParticleType;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
@@ -50,7 +51,7 @@ public final class FMLClientSetupEventHandler {
    */
   @SubscribeEvent
   public static void registerParticleProviders(RegisterParticleProvidersEvent event) {
-    FireManager.getComponentList(Fire.Component.FLAME_PARTICLE).forEach(flame -> event.registerSpriteSet(flame, FlameParticle.Provider::new));
+    FireManager.getComponentList(Fire.Component.FLAME_PARTICLE).stream().filter(SimpleParticleType.class::isInstance).forEach(flame -> event.registerSpriteSet((SimpleParticleType) flame, FlameParticle.Provider::new));
   }
 
   /**
