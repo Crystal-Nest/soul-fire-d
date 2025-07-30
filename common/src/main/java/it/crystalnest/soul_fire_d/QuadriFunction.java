c72954b3530e92dd3d43dc0a52cdbf6f57a67bfa
Riccardo Chimisso <crystal.aranea@gmail.com>
2025-07-30 19:44:32 +0200

    Update QuadriFunction.java

--- common/src/main/java/it/crystalnest/soul_fire_d/QuadriFunction.java
+++ common/src/main/java/it/crystalnest/soul_fire_d/QuadriFunction.java
@@ -10,7 +10,7 @@ import java.util.function.Function;
  * @param <T> the type of the first argument to the function.
  * @param <U> the type of the second argument to the function.
  * @param <V> the type of the third argument to the function.
- * @param <W> the type of the third argument to the function.
+ * @param <W> the type of the fourth argument to the function.
  * @param <R> the type of the result of the function.
  */
 @FunctionalInterface
