be6e852d516840ad5bf810216450cbd1d52ffee9
Crystal Spider <riccardo.chimisso@gmail.com>
2026-04-06 16:06:42 +0200

    Removed useless reference to NeoForge mod bus.

--- neoforge/src/main/java/it/crystalnest/soul_fire_d/ModLoader.java
+++ neoforge/src/main/java/it/crystalnest/soul_fire_d/ModLoader.java
@@ -12,27 +12,12 @@ import org.jetbrains.annotations.ApiStatus;
 @Mod(Constants.MOD_ID)
 public final class ModLoader {
   /**
-   * NeoForge mod event bus.
-   */
-  private static IEventBus bus;
-
-  /**
    * Mod initialization.
    *
    * @param bus Event bus.
    */
   public ModLoader(IEventBus bus) {
-    ModLoader.bus = bus;
     CommonModLoader.init();
     LootRegistry.register(bus);
-  }
-
-  /**
-   * Returns the event {@link #bus}.
-   *
-   * @return the event {@link #bus}.
-   */
-  public static IEventBus getBus() {
-    return bus;
   }
 }
